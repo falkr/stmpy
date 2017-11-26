d5e6bca09390189a30bc3771e80003a9f6df1362
Frank Alexander Kraemer <kraemer@ntnu.no>
2017-11-26 21:54:58 +0100

    Version 0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'stmpy',
+  packages = ['stmpy'],
+  version = '0.1',
+  description = 'Support for simple state machines',
+  author = 'Frank Alexander Kraemer',
+  author_email = 'kraemer.frank@gmail.com',
+  url = 'https://github.com/falkr/stmpy',
+  download_url = 'https://github.com/falkr/stmpy/archive/0.1.tar.gz',
+  keywords = ['state machines', 'stm', 'automata'],
+  classifiers = [],
+)
