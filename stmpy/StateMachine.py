27df46f958983433195180e42c15e3b93e8e370f
Frank Alexander Kraemer <kraemer@ntnu.no>
2017-12-30 13:58:36 +0100

    Changed declaration of initial transitions.

--- stmpy/StateMachine.py
+++ stmpy/StateMachine.py
@@ -6,35 +6,41 @@ def _tid(state_id, event_id):
 class StateMachine:
 
     def _parse_transitions(self, transitions):
+        self._intial_transition = None
         for transition_string in transitions:
             t_dict = transition_string #ast.literal_eval(transition_string)
             # TODO error handling: string may be written in a wrong way
-            trigger = t_dict['trigger']
             source = t_dict['source']
             target = t_dict['target']
             if 'effect' in t_dict:
                 effect = t_dict['effect']
             else:
                 effect = ''
-            t_id = _tid(source, trigger)
-            transition = _Transition(trigger, source, target, effect)
-            # TODO error handling: what if several transition with same id start from same source state?
-            self._table[t_id] = transition
+            if source is 'initial':
+                self._intial_transition = _Transition(None, source, target, effect)
+            else:
+                trigger = t_dict['trigger']
+                t_id = _tid(source, trigger)
+                transition = _Transition(trigger, source, target, effect)
+                # TODO error handling: what if several transition with same id start from same source state?
+                self._table[t_id] = transition
+        if self._intial_transition is None:
+            pass
+            # TODO raise exception for missing initial transition
 
     def _parse_states(self, states):
         for s_dict in states:
             entry = s_dict['entry']
             exit = s_dict['exit']
             name = s_dict['name']
+            # initial state cannot be detailed
             self._states[name] = _State(entry, exit)
 
 
-    def __init__(self, first_state, transitions, obj, stm_id, initial_effects='', states=[]):
-        self._first_state = first_state
-        self._state = None
+    def __init__(self, name, transitions, obj, states=[]):
+        self._state = 'initial'
         self._obj = obj
-        self._initial_effects = initial_effects.split()
-        self._id = stm_id
+        self._id = name
         self._table = {}
         self._states = {}
         self._parse_states(states)
@@ -86,12 +92,8 @@ class StateMachine:
 
 
     def _execute_transition(self, event_id, args, kwargs):
-        # execute the initial transition
-        if self._state is None:
-            if self._initial_effects is not None:
-                for function in self._initial_effects:
-                    self._run_function(self._obj, function, args=[], kwargs={})
-            self._enter_state(self._first_state)
+        if self._state is 'initial':
+            transition = self._intial_transition
         else:
             t_id = _tid(self._state, event_id)
             if t_id not in self._table:
@@ -100,10 +102,11 @@ class StateMachine:
             else:
                 transition = self._table[t_id]
                 self._exit_state(self._state)
-                for function in transition.effect:
-                    self._run_function(self._obj, function, args, kwargs)
-                # go into the next state
-                self._enter_state(transition.target)
+        for function in transition.effect:
+            self._run_function(self._obj, function, args, kwargs)
+        # go into the next state
+        self._enter_state(transition.target)
+
 
     def start_timer(self, timer_id, timeout):
         self._scheduler._start_timer(timer_id, timeout, self)
