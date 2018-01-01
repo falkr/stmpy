1a5e7c2e4a0d5db8e42cd63a3c5508e22f67639f
Frank Alexander Kraemer <kraemer@ntnu.no>
2018-01-01 20:22:52 +0100

    Single module file.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from distutils.core import setup
 setup(
-  name = 'stmpy',
-  packages = ['stmpy'],
-  version = '0.2',
-  description = 'Support for simple state machines',
-  author = 'Frank Alexander Kraemer',
-  author_email = 'kraemer.frank@gmail.com',
+  name='stmpy',
+  packages=['stmpy'],
+  version='0.2.4',
+  description='Support for simple state machines',
+  author='Frank Alexander Kraemer',
+  author_email='kraemer.frank@gmail.com',
   license='MIT',
-  url = 'https://github.com/falkr/stmpy',
-  download_url = 'https://github.com/falkr/stmpy/archive/0.2.tar.gz',
-  keywords = ['state machines', 'stm', 'automata'],
-  classifiers = [
+  url='https://github.com/falkr/stmpy',
+  download_url='https://github.com/falkr/stmpy/archive/0.2.tar.gz',
+  keywords=['state machines', 'stm', 'automata'],
+  classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Education',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',],
+        'Programming Language :: Python :: 3.5']
 )
